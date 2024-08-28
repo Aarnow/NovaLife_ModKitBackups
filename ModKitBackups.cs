c9390c15e058e1795f4835387a87b3af30fc0248
Aarnow <arnaudfgr.dev@gmail.com>
2024-08-28 15:13:30 +0200

    Initial Project Files

---
+++ ModKitBackups.cs
@@ -0,0 +1,53 @@
+﻿using Life;
+using ModKit.Helper;
+using ModKit.Interfaces;
+using ModKit.Internal;
+using ModKit.Utils;
+using System.Collections;
+using System.IO;
+
+namespace ModKitBackups
+{
+    public class ModKitBackups : ModKit.ModKit
+    {
+        public ModKitBackups(IGameAPI api) : base(api)
+        {
+            PluginInformations = new PluginInformations(AssemblyHelper.GetName(), "1.0.0", "Aarnow");
+        }
+
+        public override void OnPluginInit()
+        {
+            base.OnPluginInit();
+
+            Nova.man.StartCoroutine(BackupCoroutine());
+
+            Logger.LogSuccess($"{PluginInformations.SourceName} v{PluginInformations.Version}", "initialisé");
+        }
+
+        private IEnumerator BackupCoroutine()
+        {
+            while (true)
+            {
+                CreateBackup();
+                yield return new UnityEngine.WaitForSeconds(10 * 60); // 60 min
+            }
+        }
+
+        private void CreateBackup()
+        {
+            try
+            {
+                string timestamp = DateUtils.GetCurrentTime().ToString();
+                string backupFileName = $"data_{timestamp}.sqlite";
+                string backupFilePath = Path.Combine(BackupPath, backupFileName);
+
+                File.Copy(Path.Combine(DirectoryPath, "data.sqlite"), backupFilePath, true);
+                Logger.LogSuccess("Backup created: ", backupFilePath);
+            }
+            catch (System.Exception ex)
+            {
+                Logger.LogError("Error creating backup:", ex.Message);
+            }
+        }
+    }
+}
